16f56fc0f558972d92dfe9988e384fe5c91544d7
931007560 <931007560@qq.com>
2020-03-01 10:22:44 +0800

    编写AdministratorController

--- opencart-adminstration-back/src/main/java/com/ltl/opencartadminstrationback/controller/AdministratorController.java
+++ opencart-adminstration-back/src/main/java/com/ltl/opencartadminstrationback/controller/AdministratorController.java
@@ -1,10 +1,13 @@
 package com.ltl.opencartadminstrationback.controller;
 
+import com.ltl.opencartadminstrationback.constant.ClientExceptionConstant;
 import com.ltl.opencartadminstrationback.dto.in.*;
-import com.ltl.opencartadminstrationback.dto.out.AdministratorGetProfileOutDTO;
-import com.ltl.opencartadminstrationback.dto.out.AdministratorListOutDTO;
-import com.ltl.opencartadminstrationback.dto.out.AdministratorShowOutDTO;
-import com.ltl.opencartadminstrationback.dto.out.PageOutDTO;
+import com.ltl.opencartadminstrationback.dto.out.*;
+import com.ltl.opencartadminstrationback.exception.ClientException;
+import com.ltl.opencartadminstrationback.po.Administrator;
+import com.ltl.opencartadminstrationback.service.AdministratorService;
+import com.ltl.opencartadminstrationback.util.JWTUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,35 +15,69 @@ import java.util.List;
 @RestController
 @RequestMapping("/administrator")
 public class AdministratorController {
+
+    @Autowired
+    private AdministratorService administratorService;
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
     /** @Author ltl
      * @Description //TODO
-     * @Date 16:36 2020/2/25
+     * @Date 10:18 2020/3/1
      * @Param [administratorLoginInDTO]
-     * @return java.lang.String
+     * @return com.ltl.opencartadminstrationback.dto.out.AdministratorLoginOutDTO
      **/
     @GetMapping("/login")
-    public String login(AdministratorLoginInDTO administratorLoginInDTO){
-        return null;
+    public AdministratorLoginOutDTO login(AdministratorLoginInDTO administratorLoginInDTO) throws ClientException {
+        Administrator administrator = administratorService.getByUsername(administratorLoginInDTO.getUsername());
+        if (administrator == null){
+            throw new ClientException(ClientExceptionConstant.ADMINISTRATOR_USERNAME_NOT_EXIST_ERRCODE, ClientExceptionConstant.ADMINISTRATOR_USERNAME_NOT_EXIST_ERRMSG);
+        }
+        String encPwdDB = administrator.getEncryptedPassword();
+        BCrypt.Result result = BCrypt.verifyer().verify(administratorLoginInDTO.getPassword().toCharArray(), encPwdDB);
+
+        if (result.verified) {
+            AdministratorLoginOutDTO administratorLoginOutDTO = jwtUtil.issueToken(administrator);
+            return administratorLoginOutDTO;
+        }else {
+            throw new ClientException(ClientExceptionConstant.ADNINISTRATOR_PASSWORD_INVALID_ERRCODE, ClientExceptionConstant.ADNINISTRATOR_PASSWORD_INVALID_ERRMSG);
+        }
     }
     /** @Author ltl
      * @Description //TODO
-     * @Date 16:36 2020/2/25
+     * @Date 10:19 2020/3/1
      * @Param [adminstratorId]
      * @return com.ltl.opencartadminstrationback.dto.out.AdministratorGetProfileOutDTO
      **/
     @GetMapping("/getProfile")
     public AdministratorGetProfileOutDTO getProfile(@RequestParam(required = false) Integer adminstratorId){
-        return null;
+        Administrator administrator = administratorService.getById(adminstratorId);
+        AdministratorGetProfileOutDTO administratorGetProfileOutDTO = new AdministratorGetProfileOutDTO();
+        administratorGetProfileOutDTO.setAdministratorId(administrator.getAdministratorId());
+        administratorGetProfileOutDTO.setUsername(administrator.getUsername());
+        administratorGetProfileOutDTO.setRealName(administrator.getRealName());
+        administratorGetProfileOutDTO.setEmail(administrator.getEmail());
+        administratorGetProfileOutDTO.setAvatarUrl(administrator.getAvatarUrl());
+        administratorGetProfileOutDTO.setCreateTimestamp(administrator.getCreateTime().getTime());
+        return administratorGetProfileOutDTO;
     }
+
     /** @Author ltl
      * @Description //TODO
-     * @Date 16:36 2020/2/25
-     * @Param [administratorUpdateProfileInDTO]
+     * @Date 10:21 2020/3/1
+     * @Param [administratorUpdateProfileInDTO, administratorId]
      * @return void
      **/
     @PostMapping("/updateProfile")
-    public void updateProfile(@RequestBody AdministratorUpdateProfileInDTO administratorUpdateProfileInDTO){
-
+    public void updateProfile(@RequestBody AdministratorUpdateProfileInDTO administratorUpdateProfileInDTO
+    ,@RequestAttribute Integer administratorId){
+        Administrator administrator = new Administrator();
+        administrator.setAdministratorId(administratorId);
+        administrator.setRealName(administratorUpdateProfileInDTO.getRealName());
+        administrator.setEmail(administratorUpdateProfileInDTO.getEmail());
+        administrator.setAvatarUrl(administratorUpdateProfileInDTO.getAvatarUrl());
+        administratorService.update(administrator);
     }
     /** @Author ltl
      * @Description //TODO
