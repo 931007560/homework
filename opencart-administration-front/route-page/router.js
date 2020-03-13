0c8406e8ed5c56a3bd111f0b88e73eb59d20e7f0
931007560 <931007560@qq.com>
2020-03-13 17:42:30 +0800

    完善menu-navigate

--- opencart-administration-front/route-page/router.js
+++ opencart-administration-front/route-page/router.js
@@ -1,6 +1,8 @@
 const routes = [
     { path: '/product/search', component: ProductSearchRoutePage },
-    { path: '/customer/search', component: CustomerSearchRoutePage }
+    { path: '/customer/search', component: CustomerSearchRoutePage },
+    { path: '/order/search', component: OrderSearchRoutePage },
+    { path: '/return/search', component: ReturnSearchRoutePage }
 ];
 
 const router = new VueRouter({
