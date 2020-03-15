df2acd53d5c5996222a9f6ba14fc5c6cd1122a8e
931007560 <931007560@qq.com>
2020-03-15 19:00:41 +0800

    完善menu-navigate

--- opencart-administration-front/route-page/router.js
+++ opencart-administration-front/route-page/router.js
@@ -1,8 +1,12 @@
 const routes = [
     { path: '/product/search', component: ProductSearchRoutePage },
     { path: '/product/create', component: ProductCreateRoutePage },
-    
+
     { path: '/customer/search', component: CustomerSearchRoutePage },
+    { path: '/customer/show/:customerId', component: CustomerShowRoutePage },
+
+    { path: '/address/index/:customerId', component: AddressIndexRoutePage },
+
     { path: '/order/search', component: OrderSearchRoutePage },
     { path: '/return/search', component: ReturnSearchRoutePage },
     { path: '/administrator/index', component: AdministratorIndexRoutePage }
